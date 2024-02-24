bb92da0a284d8bfc8faac5d705b9656973062c64
JASTI SAI PRIYA <160637388+saipriyajasti@users.noreply.github.com>
2024-02-25 00:43:42 +0530

    Add files via upload

---
+++ ksp_task4.js
@@ -0,0 +1,31 @@
+//Alert Message
+alert("Hello, Welcome to this Page!!!");
+
+//To print something
+console.log("MY NAME IS PRIYA.....");
+
+//THIS IS A SINGLE LINE COMMENT
+
+
+/*
+THIS IS
+   A MULTI LINE COMMENT
+*/
+
+//Variables
+let number1=10;  //this is int datatype
+let number2=2.9;  //this is float datatype
+let name="priya";   //this is a string
+let character="J";   //this is a character
+let bool=false;   //this is a boolean datatype
+
+//const
+let number3=7.2;
+const name1="jasti";
+var number4=99;
+number5=100;
+console.log(number3);
+console.log("number3");
+console.log(name1); 
+console.log(number4);
+console.log(number5); 
